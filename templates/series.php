cc2b5136e448e298127c9a8ac0bdaa6e7968f838
Joshua Pangborn <joshua.pangborn@gmail.com>
2017-01-07 20:52:02 -0500

    Remove old template code

--- templates/series.php
+++ templates/series.php
@@ -59,24 +59,3 @@
     </div>
 
 <?php snippet('html-end') ?>
-
-
-
-
-
-
-
-
-
-<?php snippet('header') ?>
-
-  <main class="ccl-main" role="main">
-
-    <h1><?= $page->title()->html() ?></h1>
-
-    <?php foreach(page(c::get('slk.main.uri'))->grandChildren()->visible()->filterBy('template', 'slk_sermon')->filterBy('series', $page->uri()) as $sermon): ?>
-    <?php snippet('sermon', array('sermon' => $sermon)) ?>
-    <?php endforeach ?>
-  </main>
-
-<?php snippet('footer') ?>
