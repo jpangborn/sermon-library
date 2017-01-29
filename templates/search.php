499c6fd4f568fa52687885cabfdd2ec49cc40542
Joshua Pangborn <joshua.pangborn@gmail.com>
2017-01-28 21:48:13 -0500

    Center search button

--- templates/search.php
+++ templates/search.php
@@ -19,13 +19,11 @@
                   <div class="row">
                     <div class="12u">
                       <input type="text" name="q" id="search-query" placeholder="Search..." value="<?= r($results, esc($results->query())) ?>">
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="12u">
-                      <ul class="actions">
-                        <li><input type="submit" class="style1" value="Search"></li>
-                      </ul>
+                      <section class="highlight">
+                        <ul class="actions">
+                          <li><input type="submit" class="style1" value="Search"></li>
+                        </ul>
+                      </section>
                     </div>
                   </div>
                 </form>
