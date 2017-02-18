1cef2b036ae35d9fdff9b767840aae48ac1f5fa0
Joshua Pangborn <joshua.pangborn@gmail.com>
2017-02-18 02:34:27 -0500

    Ensure only sermons with audio are in podcast

--- controllers/library.rss.php
+++ controllers/library.rss.php
@@ -1,6 +1,6 @@
 <?php
   return function($site, $pages, $page) {
-    $sermons = $page->grandChildren()->visible()->filterBy('template', 'sermon')->flip()->limit(25);
+    $sermons = $page->grandChildren()->visible()->filterBy('template', 'sermon')->filterBy('hasAudio', '>', '0')->flip()->limit(25);
 
     return compact('sermons');
   };
